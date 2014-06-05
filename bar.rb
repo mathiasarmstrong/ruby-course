ac80974711da2bf4b716a33ba5e03865249eb2a3
Jered McCullough <mathias.armstrong@gmail.com>
2014-06-05 16:37:12 -0700

    solved all of the issue and wrote a few tests of my own and solved them as well

--- bar.rb
+++ bar.rb
@@ -1,4 +1,40 @@
 require 'time' # you're gonna need it
 
 class Bar
+ attr_reader :name, :menu_items
+  def initialize(name)
+    @name=name
+    @menu_items=[]
+    @happy_discount=0
+  end
+
+  def add_menu_item(name, price)
+    menu_items<<Item.new(name,price)
+  end
+
+  def happy_discount
+    self.happy_hour? ? (return @happy_discount) : (return 0)
+  end
+  def happy_discount=(amount)
+    if amount<=1
+      if amount>=0
+        @happy_discount=amount
+      else
+        @happy_discount=0
+      end
+    else
+      @happy_discount=1
+    end
+  end
+  def happy_hour?
+    Time.now.hour==3
+  end
+end
+class Item
+  attr_reader :name, :price
+  def initialize(name, price)
+    @name=name
+    @price=price
+    [@name,@price]
+  end
 end
