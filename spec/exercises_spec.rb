3749c62ac7f1df555934510138b06f98f65229a3
Jered McCullough <mathias.armstrong@gmail.com>
2014-06-06 14:54:59 -0700

    I wrote the method for ex2 and tested it on an array with no element and an array with several elements

--- spec/exercises_spec.rb
+++ spec/exercises_spec.rb
@@ -33,6 +33,19 @@ describe Exercises  do
     end
   end
 
-
+  describe 'ex2' do
+    context 'array has no elements' do
+      it 'returns nil' do
+        x=Exercises.ex2([])
+        expect(x).to be_nil
+      end
+    end
+    context "array has several elements" do
+      it "returns the second element of the array" do
+        x=Exercises.ex2([1,["friends","girls"],{"dog"=>2},"this is interesting"])
+        expect(x).to eq(["friends","girls"])
+      end
+    end
+  end
 
 end
