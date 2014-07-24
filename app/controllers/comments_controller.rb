0e76be2bab452112105f76ba3fde378dd8852a8c
Jered McCullough <mathias.armstrong@gmail.com>
2014-07-23 17:44:37 -0700

    transferred comment system from movies to sounds

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,12 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to @comment.video
+      redirect_to "/#{@comment.commentable_type.downcase.pluralize}/#{@comment.commentable_id}"
     end
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:message, :commentable_id)
+    params.require(:comment).permit(:message, :commentable_id, :commentable_type)
   end
 end
--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -6,6 +6,8 @@ class SoundsController < ApplicationController
 
   def show
     @sound = Sound.find(params[:id])
+    @comments = @sound.comments
+    @comment = @sound.comments.new
   end
 
   def destroy
